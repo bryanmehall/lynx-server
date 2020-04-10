32263a2f3203fe753c9a09588147eb39880f9536
Bryan Mehall <bryanmehall@gmail.com>
2020-04-10 17:18:33 -0600

    build basic webpage

--- server/lynx-webserver.ts
+++ server/lynx-webserver.ts
@@ -2,32 +2,46 @@ import http from 'http'
 import fs from 'fs'
 import path from 'path'
 
-const filePath = path.join(__dirname, '../client/index.html')
+
 const PORT = process.env.PORT || 3000
 const NODE_ENV = process.env.NODE_ENV || "development"
 
 const server = http.createServer((req, res) => {
     // Server-sent events endpoint
-    if (req.url === '/events') {
-        res.writeHead(200, {
-            'Content-Type': 'text/event-stream',
-            'Cache-Control': 'no-cache',
-            'Connection': 'keep-alive',
-        });
-        const refreshRate = 5000; // in milliseconds
-        const id = Date.now();
-        const data = `Hello World!!! ${id}`
-        const message =
-        `retry: ${refreshRate}\nid:${id}\ndata: ${data}\n\n`
-        res.end(message)
-        return;
+    if (req.url === '/subscribe') {
+        sendEvent(res)
     } else {
-        fs.readFile(filePath, (err, data) => {
-            res.writeHead(200, {'Content-Type': 'text/html'})
-            res.end(data)
-        })
+        console.log(req.url)
+        returnStatic(req, res)
     }
 })
+
+const sendEvent = (res: http.ServerResponse) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+    });
+    const refreshRate = 5000; // in milliseconds
+    const id = Date.now();
+    const data = `Hello World!!! ${id}`
+    const message =
+    `retry: ${refreshRate}\nid:${id}\ndata: ${data}\n\n`
+    res.end(message)
+}
+
+const returnStatic = (req, res: http.ServerResponse) => {
+    const fileName = req.url === '/' ? './index.html' : req.url
+    const filePath = path.join(__dirname, fileName)
+    fs.readFile(filePath, (err, data) => {
+        if (err){
+            console.log(`Error fetching ${fileName}`, err)
+        } else {
+            res.writeHead(200, {})
+            res.end(data)
+        }
+    })
+}
 
 
 
